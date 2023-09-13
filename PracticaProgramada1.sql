e545f936ff8125526fb17eb6c647cd004f0d7cab
Miriam Calvo <49732161+Miriam0909@users.noreply.github.com>
2023-09-13 17:25:48 -0600

    Update PracticaProgramada1.sql

--- PracticaProgramada1.sql
+++ PracticaProgramada1.sql
@@ -1,19 +1,19 @@
---- Creación de la base de datos
+--- CreaciÃ³n de la base de datos
 CREATE DATABASE PracticaProgramada
 go
 
 --- Uso de la base
 use PracticaProgramada
 
---- Creación de tablas para cada entidad 
-
+--- CreaciÃ³n de tablas para cada entidad 
+--- Tabla para Territorio
 CREATE TABLE Territorio (
     ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     provincia VARCHAR(50) NOT NULL,
     canton VARCHAR(50) NOT NULL,
     distrito VARCHAR(50) NOT NULL
 )
-
+--- Tabla para Proveedor
 CREATE TABLE Proveedor (
     cedula VARCHAR(20) NOT NULL PRIMARY KEY,
     tipocedula BINARY NOT NULL,
@@ -24,19 +24,19 @@ CREATE TABLE Proveedor (
     CONSTRAINT FK_TERRITORIO FOREIGN KEY (idTerritorio) REFERENCES Territorio(ID)
 )
 
-
+--- Tabla para Categoria
 CREATE TABLE Categoria (
     ID INT PRIMARY KEY IDENTITY(1,1),
     nombre VARCHAR(30) NOT NULL
 )
-
+--- Tabla para Subcategoria
 CREATE TABLE Subcategoria (
     ID INT PRIMARY KEY NOT NULL IDENTITY(1,1),
     nombre VARCHAR(30) NOT NULL,
 	idcategoria INT NOT NULL,
 	CONSTRAINT FK_CATEGORIA FOREIGN KEY (idcategoria) REFERENCES Categoria(ID)
 )
-
+--- Tabla para Cliente
 CREATE TABLE Cliente (
     ID INT PRIMARY KEY IDENTITY(1,1),
     cedula VARCHAR(30) NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE Cliente (
     direccionfisica VARCHAR(200) NOT NULL,
     correoelectronico VARCHAR(40) NOT NULL
 )
-
+--- Tabla para Producto
 CREATE TABLE Producto (
     ProductoID INT PRIMARY KEY IDENTITY(1,1),
     IDUniversal VARCHAR(15) NOT NULL,
@@ -57,7 +57,7 @@ CREATE TABLE Producto (
 	CONSTRAINT FK_PROVEEDOR FOREIGN KEY (idProveedor) REFERENCES Proveedor(cedula),
 	CONSTRAINT FK_SUBCATEGORIA FOREIGN KEY (idSubcategoria) REFERENCES Subcategoria(ID),
 )
-
+--- Tabla para Factura
 CREATE TABLE Factura (
     ID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	fechacompra DATE NOT NULL, 
@@ -69,12 +69,12 @@ CREATE TABLE Factura (
 	idCliente INT NOT NULL,  
     CONSTRAINT FK_CLIENTE FOREIGN KEY (idCliente) REFERENCES Cliente(ID),
 )
-
+--- Tabla intermedia
 CREATE TABLE detallefactura (
     idProducto INT NOT NULL,
     idFactura INT NOT NULL,
-	cantidadproducto INT NOT NULL,
-	total FLOAT NOT NULL,
+    cantidadproducto INT NOT NULL,
+    total FLOAT NOT NULL,
     CONSTRAINT FK_DETALLEPRODUCTO FOREIGN KEY (idProducto) REFERENCES Producto(ProductoID),
     CONSTRAINT FK_DETALLEFACTURA FOREIGN KEY (idFactura) REFERENCES Factura(ID)
 );
@@ -83,19 +83,19 @@ CREATE TABLE detallefactura (
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('San José', 'San José', 'Catedral')
+	VALUES ('San JosÃ©', 'San JosÃ©', 'Catedral')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('San José', 'San José', 'Escazú')
+	VALUES ('San JosÃ©', 'San JosÃ©', 'EscazÃº')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('San José', 'Desamparados', 'San Miguel')
+	VALUES ('San JosÃ©', 'Desamparados', 'San Miguel')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('San José', 'Desamparados', 'San Juan de Dios')
+	VALUES ('San JosÃ©', 'Desamparados', 'San Juan de Dios')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
@@ -139,11 +139,11 @@ INSERT INTO Territorio
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Cartago', 'La Unión', 'La Unión')
+	VALUES ('Cartago', 'La UniÃ³n', 'La UniÃ³n')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Cartago', 'La Unión', 'Tres Ríos')
+	VALUES ('Cartago', 'La UniÃ³n', 'Tres RÃ­os')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
@@ -159,7 +159,7 @@ INSERT INTO Territorio
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Guanacaste', 'Santa Cruz', 'Bolsón')
+	VALUES ('Guanacaste', 'Santa Cruz', 'BolsÃ³n')
 
 INSERT INTO Territorio 
 (provincia, canton, distrito)
@@ -175,23 +175,23 @@ INSERT INTO Territorio
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Puntarenas', 'Golfito', 'Puerto Jiménez')
+	VALUES ('Puntarenas', 'Golfito', 'Puerto JimÃ©nez')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Limón', 'Limón', 'Limón')
+	VALUES ('LimÃ³n', 'LimÃ³n', 'LimÃ³n')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Limón', 'Limón', 'Valle La Estrella')
+	VALUES ('LimÃ³n', 'LimÃ³n', 'Valle La Estrella')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Limón', 'Pococí', 'Guápiles')
+	VALUES ('LimÃ³n', 'PococÃ­', 'GuÃ¡piles')
 
 INSERT INTO Territorio 
 	(provincia, canton, distrito)
-	VALUES ('Limón', 'Pococí', 'Rita')
+	VALUES ('LimÃ³n', 'PococÃ­', 'Rita')
 
 
 INSERT INTO Proveedor (cedula, tipocedula, nombre, correo, telefono, idTerritorio)
@@ -203,12 +203,12 @@ VALUES
     ('777888999', 1, 'Costa Rica Beverages', 'crbeverages@example.com', '89899999', 14), 
     ('222333444', 0, 'Guanacaste Distributors', 'guanacastedist@example.com', '47487433', 19), 
     ('555666777', 1, 'Puntarenas Importers', 'puntarenasimp@example.com', '25116666', 22), 
-    ('888999000', 0, 'Limón Exports', 'limonexports@example.com', '25112728', 27)  
+    ('888999000', 0, 'LimÃ³n Exports', 'limonexports@example.com', '25112728', 27)  
 
 
 INSERT INTO Categoria (nombre)
 VALUES
-    ('Electrónica'),
+    ('ElectrÃ³nica'),
     ('Ropa'),
     ('Hogar'),
     ('Alimentos'),
@@ -218,9 +218,9 @@ VALUES
 
 INSERT INTO Subcategoria (nombre, idcategoria)
 VALUES
-    ('Electrónica de Consumo', 1),  
+    ('ElectrÃ³nica de Consumo', 1),  
     ('Ropa de Hombre', 2),          
-    ('Electrodomésticos', 3),       
+    ('ElectrodomÃ©sticos', 3),       
     ('Alimentos No Perecederos', 4),
     ('Juguetes Educativos', 5),     
     ('Piezas de Repuesto', 6),      
@@ -228,32 +228,32 @@ VALUES
 
 INSERT INTO Cliente (cedula, nombre, direccionfisica, correoelectronico)
 VALUES
-    ('123456789', 'Ana González', 'Avenida 1, San José', 'ana.gonzalez@example.com'),
-    ('987654321', 'Carlos Rodríguez', 'Calle Principal, Alajuela', 'carlos.rodriguez@example.com'),
-    ('456789123', 'María Fernández', 'Calle 2, Heredia', 'maria.fernandez@example.com'),
-    ('789123456', 'Luis Sánchez', 'Avenida Central, Cartago', 'luis.sanchez@example.com'),
+    ('123456789', 'Ana GonzÃ¡lez', 'Avenida 1, San JosÃ©', 'ana.gonzalez@example.com'),
+    ('987654321', 'Carlos RodrÃ­guez', 'Calle Principal, Alajuela', 'carlos.rodriguez@example.com'),
+    ('456789123', 'MarÃ­a FernÃ¡ndez', 'Calle 2, Heredia', 'maria.fernandez@example.com'),
+    ('789123456', 'Luis SÃ¡nchez', 'Avenida Central, Cartago', 'luis.sanchez@example.com'),
     ('234567890', 'Laura Morales', 'Calle 3, Guanacaste', 'laura.morales@example.com'),
-    ('567890123', 'José Pérez', 'Avenida 4, Puntarenas', 'jose.perez@example.com'),
-    ('345678912', 'Sofía Ramírez', 'Calle 5, Limón', 'sofia.ramirez@example.com')
+    ('567890123', 'JosÃ© PÃ©rez', 'Avenida 4, Puntarenas', 'jose.perez@example.com'),
+    ('345678912', 'SofÃ­a RamÃ­rez', 'Calle 5, LimÃ³n', 'sofia.ramirez@example.com')
 
 INSERT INTO Producto (IDUniversal, nombre, precio, tamano, color, idProveedor, idSubcategoria)
 VALUES
     ('A123', 'Televisor LED 55 pulgadas', 599.99, '55x30x5 cm', 'Negro', '123456789', 1),  
     ('B456', 'Laptop HP Envy', 899.00, '13.3 pulgadas', 'Plateado', '987654321', 1),     
     ('C789', 'Refrigeradora Samsung', 799.99, '180x70x70 cm', 'Blanco', '456789123', 2),
-    ('D101', 'Teléfono inteligente iPhone 12', 799.00, '6.1 pulgadas', 'Gris Espacial', '789123456', 1), 
+    ('D101', 'TelÃ©fono inteligente iPhone 12', 799.00, '6.1 pulgadas', 'Gris Espacial', '789123456', 1), 
     ('E202', 'Lavadora LG', 499.99, '85x60x60 cm', 'Plateado', '234567890', 2),           
     ('F303', 'Tablet Samsung Galaxy Tab', 249.00, '10.1 pulgadas', 'Negro', '567890123', 1),
-    ('G404', 'Cámara Canon EOS Rebel', 649.00, '18 MP', 'Negro', '345678912', 3);         
+    ('G404', 'CÃ¡mara Canon EOS Rebel', 649.00, '18 MP', 'Negro', '345678912', 3);         
 
 INSERT INTO Factura (fechacompra, impuesto, descuento, unidadescompradas, info, precio, idCliente)
 VALUES
-    ('2023-09-01', 15.00, 10.00, 3, 'Compra de productos electrónicos', 599.99, 1), 
-    ('2023-09-02', 25.00, 5.00, 2, 'Compra de electrodomésticos', 899.00, 2),        
-    ('2023-09-03', 10.00, 0.00, 1, 'Compra de teléfono inteligente', 799.00, 3),    
+    ('2023-09-01', 15.00, 10.00, 3, 'Compra de productos electrÃ³nicos', 599.99, 1), 
+    ('2023-09-02', 25.00, 5.00, 2, 'Compra de electrodomÃ©sticos', 899.00, 2),        
+    ('2023-09-03', 10.00, 0.00, 1, 'Compra de telÃ©fono inteligente', 799.00, 3),    
     ('2023-09-04', 20.00, 15.00, 2, 'Compra de lavadora', 499.99, 4),                
     ('2023-09-05', 8.00, 5.00, 1, 'Compra de tablet', 249.00, 5),                  
-    ('2023-09-06', 12.00, 8.00, 1, 'Compra de cámara', 649.00, 6);                  
+    ('2023-09-06', 12.00, 8.00, 1, 'Compra de cÃ¡mara', 649.00, 6);                  
 
 
 INSERT INTO detallefactura (idProducto, idFactura, cantidadproducto, total)
@@ -269,32 +269,32 @@ VALUES
 
 --- Consultas
 
---- ¿Cuánto dinero se ha vendido en total por mes?
+--- Â¿CuÃ¡nto dinero se ha vendido en total por mes?
 SELECT
     MONTH(fechacompra) AS Mes,
-    YEAR(fechacompra) AS Año,
+    YEAR(fechacompra) AS AÃ±o,
     SUM(precio) AS TotalVentas
 FROM Factura
 GROUP BY YEAR(fechacompra), MONTH(fechacompra)
-ORDER BY Año, Mes;
+ORDER BY AÃ±o, Mes;
 
 
---- ¿Cuántos productos diferentes tiene la empresa?
+--- Â¿CuÃ¡ntos productos diferentes tiene la empresa?
 SELECT COUNT(DISTINCT ProductoID) AS TotalProductosDiferentes
 FROM Producto;
 
---- ¿Cuáles son los proveedores a los que más unidades de productos se les compra?
+--- Â¿CuÃ¡les son los proveedores a los que mÃ¡s unidades de productos se les compra?
 SELECT
     P.nombre AS Proveedor,
     SUM(DF.cantidadproducto) AS TotalUnidadesCompradas
 FROM Proveedor P
-INNER JOIN Producto PR ON P.cedula = PR.idProveedor   -- combina registros de dos o más tablas en función de la condición. 
+INNER JOIN Producto PR ON P.cedula = PR.idProveedor   -- combina registros de dos o mÃ¡s tablas en funciÃ³n de la condiciÃ³n. 
 INNER JOIN detallefactura DF ON PR.ProductoID = DF.idProducto
 GROUP BY P.nombre
 ORDER BY TotalUnidadesCompradas DESC;
 
 
---- ¿Cuáles son los clientes a los que más unidades de productos se les vende?
+--- Â¿CuÃ¡les son los clientes a los que mÃ¡s unidades de productos se les vende?
 SELECT
     C.nombre AS Cliente,
     SUM(DF.cantidadproducto) AS TotalUnidadesVendidas
@@ -304,7 +304,7 @@ INNER JOIN detallefactura DF ON F.ID = DF.idFactura
 GROUP BY C.nombre
 ORDER BY TotalUnidadesVendidas DESC;
 
---- ¿Qué categoría de producto vende menos?
+--- Â¿QuÃ© categorÃ­a de producto vende menos?
 
 SELECT
     C.nombre AS Categoria,
